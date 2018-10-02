018d94b710e0cc19a71e24dbd3bd28417a649fd0
rafehsaeed <rafeh_saeed@hotmail.ca>
2018-10-02 11:53:21 -0400

    setting up the status rotate method in the controller

--- app/controllers/data_encrypting_keys_controller.rb
+++ app/controllers/data_encrypting_keys_controller.rb
@@ -2,12 +2,33 @@ class DataEncryptingKeysController < ApplicationController
 
   # Trigger the rotation mechanism here 
   def rotate
-    RotationWorker.perform_async()
+
+  	stats = Sidekiq::Stats.new 
+  	if stats.enqueued == 0 
+  		RotationWorker.perform_async()
+  			render json: { message: "Key rotation has been queued"},
+	    	status: :success
+  	else
+  		status()
+  	end
+
   end
 
   # Return the status here 
   def status
-    
+
+  	stats = Sidekiq::Stats.new
+  	if stats.enqueued == 0 
+		render json: { message: "No key rotation queued or in progress"},
+	    status: :unprocessable_entity
+	end 
+
+# 	check to see if worker running if yes send the message that already in progress
+	# workers = Sidekiq::Workers.new
+	# workers.each do |_process_id, _thread_id, work|
+ #  		p work
+	# end
+
   end
    
 end
